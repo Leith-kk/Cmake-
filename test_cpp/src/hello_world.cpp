ca15084aa36534d8685c9ae94d03cf0968d1b3ca
Leith-kk <132372265+Leith-kk@users.noreply.github.com>
2024-07-08 11:50:20 +0800

    Add files via upload

---
+++ test_cpp/src/hello_world.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a = 100;
+    int b = 1000;
+    int c = a + b;
+    char m = 'l';
+    string s ="hello ";
+
+    
+    cout << s << m << endl;
+    cout << c << endl;
+    cout <<"hello world!"<<endl;
+}
